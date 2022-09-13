1f0960942401bd887397c14a8d15264476292c4b
Clarence <47097322+clarence1979@users.noreply.github.com>
2022-09-14 00:57:17 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,52 @@
+input.onButtonPressed(Button.A, function () {
+    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+})
 radio.onReceivedValue(function (name, value) {
-    if (name == "y") {
-        if (("y" as any) < (0 as any)) {
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-        } else {
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                `)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 100)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-        }
-    } else if (name == "x") {
-        if (("x" as any) < (0 as any)) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
-            basic.showLeds(`
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                `)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-        } else {
-        	
-        }
+    if (name == "y" && value < 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+    } else if (name == "y" && value > 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 100)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
+    } else if (name == "x" && value > 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (name == "x" && value < 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Happy)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
     }
 })
 radio.setGroup(1)
